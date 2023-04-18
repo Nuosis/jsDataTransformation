cfc7805caffed2a2588da0709d89ca2eec8dea48
Marcus Swift <marcus@claritybusinesssolutions.ca>
2023-04-18 08:56:56 -0700

    invoiceRecordsReturn

--- src/index.js
+++ src/index.js
@@ -64,33 +64,21 @@ window.getInvoiceRecordSUM = (json) => {
 };
 
 window.getSpecifiedInvoiceRecords = (json) => {
-  const data = JSON.parse(json);
-  console.log(data);
+  const input = JSON.parse(json);
+  //console.log(input);
+  const custID = input.id;
+  console.log(custID);
+  const data = input.data;
+  //console.log(data);
 
-  //get all customers
-  const custArray = [... new Set( data.map ( obj => obj.fieldData.customerName ))].sort();
+  //get customer's records from fmData
+  const customerRecords = data.filter(function(records) {    return records.fieldData._custID === custID});
+  console.log(customerRecords);
   
-  //for each customer
-  const result = custArray.map(function(record) {
-    //get customer's records from fmData
-    const custName = record;
-    //console.log(custName);
-    const customerRecords = data.filter(function(records) {    return records.fieldData.customerName === record});
-    //console.log(customerRecords);
-    const ID = customerRecords[0].fieldData.__ID;
-    //console.log(ID);
+  const invoiceIDS =  customerRecords.map(function(record) {
+    return record.fieldData.__ID
+  });
 
-    //get all recordIDS
-    const invoiceIDS =  customerRecords.map(function(record) {
-      return record.fieldData.__ID
-    });
-
-    const obj = { id: `${invoiceIDS}`};
-    //console.log(obj);
-
-    return obj;
-    });
-
-    FileMaker.PerformScript("jsTransform . setJSResult", JSON.stringify(result));
+  FileMaker.PerformScript("jsTransform . setJSResult", JSON.stringify(invoiceIDS));
 
 };
